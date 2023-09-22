567de5761f56614483762e191559a2b82fa1967e
Jay-Agui <145313034+Jay-Agui@users.noreply.github.com>
2023-09-22 08:00:02 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body>
+    <header>
+      <nav class="bg-white flex justify-around p-3">
+        <h1 class="font-bold text-lg">Jay Mark Agui</h1>
+        <ul class="navbar flex justify-center">
+          <li><a class="px-3" href="//index.php">Home</a></li>
+          <li><a class="px-3" href="index.php/home/test">Contact</a></li>
+          <li><a class="px-3" href="index.php/home/resume">Resume</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
